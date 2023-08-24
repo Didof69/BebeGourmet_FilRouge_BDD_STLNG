030aa117ec0c89b571bdc729f62a6dbb1333cf26
Lydie Chevalier <didof69@hotmail.fr>
2023-08-24 21:26:32 +0200

    ajour insert enfant

--- script_insert.sql
+++ script_insert.sql
@@ -349,3 +349,12 @@ INSERT INTO utilisateur (nom, prenom, email, mot_de_passe,admin) VALUES ('Durand
 ('Patel', 'Liam', 'patelliam@email.fr', 'mot_de_passe123',false),
 ('Chen', 'Sofia', 'S_chen@email.fr', 'mot_de_passe123',false),
 ('Khan', 'Ethan', 'ethan.k@email.fr', 'mot_de_passe123',false);
+
+INSERT INTO enfant (prenom, date_naissance, id_utilisateur) VALUES ('Jade', '12/12/2022', 2),
+('Timéo', '08/04/2021', 2),
+('Marius', '05/01/2023', 3),
+('Sarah', '04/12/2021', 4),
+('Mathilde', '27/06/2023', 5),
+('Leila', '09/03/2023', 6),
+('Amine', '24/11/2021', 6),
+('Boris', '12/10/2022', 7);
--- table_enfant.sql
+++ table_enfant.sql
@@ -1,7 +1,16 @@
-CREATE TABLE Enfant 
+CREATE TABLE enfant 
 (
     id PK SERIAL,
     prenom VARCHAR(255) NOT NULL,
     date_naissance DATE NOT NULL,
     id_utilisateur FK INT NOT NULL
 );
+
+INSERT INTO enfant (prenom, date_naissance, id_utilisateur) VALUES ('Jade', '12/12/2022', 2),
+('Timéo', '08/04/2021', 2),
+('Marius', '05/01/2023', 3),
+('Sarah', '04/12/2021', 4),
+('Mathilde', '27/06/2023', 5),
+('Leila', '09/03/2023', 6),
+('Amine', '24/11/2021', 6),
+('Boris', '12/10/2022', 7);
