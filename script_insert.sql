7b8c79b53d65b3648b3bbd9e0f91f174a2ea1b09
Lydie Chevalier <didof69@hotmail.fr>
2023-08-24 21:17:45 +0200

    ajout insert utilisateur

--- script_insert.sql
+++ script_insert.sql
@@ -342,3 +342,10 @@ INSERT INTO restriction (libelle) VALUES ('Végétarien et Vegan'),
 ('Sulfites'),
 ('Fruits à coques');
 
+INSERT INTO utilisateur (nom, prenom, email, mot_de_passe,admin) VALUES ('Durand', 'Paul', 'durand_paul@email.fr', 'mot_de_passe123',true),
+('Dupond', 'Lucie', 'dupond-lucie@email.fr', 'mot_de_passe123',false),
+('Johnson', 'Alex', 'alexjohnsonl@email.fr', 'mot_de_passe123',false),
+('Rodriguez', 'Emily', 'e.rodriguez@email.fr', 'mot_de_passe123',false),
+('Patel', 'Liam', 'patelliam@email.fr', 'mot_de_passe123',false),
+('Chen', 'Sofia', 'S_chen@email.fr', 'mot_de_passe123',false),
+('Khan', 'Ethan', 'ethan.k@email.fr', 'mot_de_passe123',false);
--- table_utilisateur.sql
+++ table_utilisateur.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Utilisateur 
+CREATE TABLE utilisateur 
 (
     id PK SERIAL,
     nom VARCHAR(255) NOT NULL,
@@ -7,3 +7,11 @@ CREATE TABLE Utilisateur
     mot_de_passe VARCHAR(15) NOT NULL,
     admin BOOLEAN
 );
+
+INSERT INTO utilisateur (nom, prenom, email, mot_de_passe,admin) VALUES ('Durand', 'Paul', 'durand_paul@email.fr', 'mot_de_passe123',true),
+('Dupond', 'Lucie', 'dupond-lucie@email.fr', 'mot_de_passe123',false),
+('Johnson', 'Alex', 'alexjohnsonl@email.fr', 'mot_de_passe123',false),
+('Rodriguez', 'Emily', 'e.rodriguez@email.fr', 'mot_de_passe123',false),
+('Patel', 'Liam', 'patelliam@email.fr', 'mot_de_passe123',false),
+('Chen', 'Sofia', 'S_chen@email.fr', 'mot_de_passe123',false),
+('Khan', 'Ethan', 'ethan.k@email.fr', 'mot_de_passe123',false);
