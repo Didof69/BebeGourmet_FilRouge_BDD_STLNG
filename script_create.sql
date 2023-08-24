1d405db3e838aa83d3009d463c858c87ae911c29
Lydie Chevalier <didof69@hotmail.fr>
2023-08-24 07:51:57 +0200

    correction des CREATEDATABASE, creation 2 scripts Create et INSERT, modif insert to

---
+++ script_create.sql
@@ -0,0 +1,75 @@
+CREATE TABLE categories 
+(
+    id SERIAL PRIMARY KEY,
+    libelle	VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE aliment 
+(
+    id SERIAL PRIMARY KEY,
+    libelle	VARCHAR(512) NOT NULL,
+    age_introduction INT NOT NULL,
+    id_categorie INT NOT NULL,
+    CONSTRAINT fk_categorie FOREIGN KEY (id_categorie) REFERENCES categories (id)
+);
+
+CREATE TABLE utilisateur 
+(
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    mot_de_passe VARCHAR(15) NOT NULL,
+    admin BOOLEAN
+);
+
+CREATE TABLE enfant 
+(
+    id SERIAL PRIMARY KEY,
+    prenom VARCHAR(255) NOT NULL,
+    date_naissance DATE NOT NULL,
+    id_utilisateur INT NOT NULL,
+    CONSTRAINT fk_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur (id)
+);
+
+CREATE TABLE restriction 
+(
+    id SERIAL PRIMARY KEY,
+    libelle	VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE posseder
+(
+    id_enfant INT NOT NULL,
+    id_restriction INT NOT NULL,
+    PRIMARY KEY (id_enfant , id_restriction),
+    CONSTRAINT fk_enfant FOREIGN KEY (id_enfant) REFERENCES enfant(id),
+    CONSTRAINT fk_restriction FOREIGN KEY (id_restriction) REFERENCES restriction(id)
+);
+
+CREATE TABLE limiter
+(
+    id_aliment INT NOT NULL,
+    id_restriction INT NOT NULL,
+    PRIMARY KEY (id_aliment , id_restriction),
+    CONSTRAINT fk_aliment FOREIGN KEY (id_aliment) REFERENCES aliment(id),
+    CONSTRAINT fk_restriction FOREIGN KEY (id_restriction) REFERENCES restriction(id)
+);
+
+CREATE TABLE saison
+(
+    id SERIAL PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE produire
+(
+    id_aliment INT NOT NULL,
+    id_saison INT NOT NULL,
+    PRIMARY KEY (id_aliment , id_saison),
+    CONSTRAINT fk_aliment FOREIGN KEY (id_aliment) REFERENCES aliment(id),
+    CONSTRAINT fk_saison FOREIGN KEY (id_saison) REFERENCES saison(id)
+);
+
+
+
--- table_categories.sql
+++ table_categories.sql
@@ -4,11 +4,11 @@ CREATE TABLE Categories
     libelle	VARCHAR(255) NOT NULL
 );
 
-INSERT INTO Categories (libelle, age_introduction) VALUES ('Légumes');
-INSERT INTO Categories (libelle, age_introduction) VALUES ('Fruits');
-INSERT INTO Categories (libelle, age_introduction) VALUES ('Féculents');
-INSERT INTO Categories (libelle, age_introduction) VALUES ('Protéines');
-INSERT INTO Categories (libelle, age_introduction) VALUES ('Laitages');
-INSERT INTO Categories (libelle, age_introduction) VALUES ('Aliments sucrés');
-INSERT INTO Categories (libelle, age_introduction) VALUES ('Matières grasses');
-INSERT INTO Categories (libelle, age_introduction) VALUES ('Epices et condiments');
+INSERT INTO Categories (libelle) VALUES ('Légumes');
+INSERT INTO Categories (libelle) VALUES ('Fruits');
+INSERT INTO Categories (libelle) VALUES ('Féculents');
+INSERT INTO Categories (libelle) VALUES ('Protéines');
+INSERT INTO Categories (libelle) VALUES ('Laitages');
+INSERT INTO Categories (libelle) VALUES ('Aliments sucrés');
+INSERT INTO Categories (libelle) VALUES ('Matières grasses');
+INSERT INTO Categories (libelle) VALUES ('Epices et condiments');
