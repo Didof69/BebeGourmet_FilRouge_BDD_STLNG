3ef44c4319f3120ccdd726ecef0a2b51ac99c6b8
Lydie Chevalier <didof69@hotmail.fr>
2023-09-07 09:28:43 +0200

    modif contrainte mot_de_passe table utilisateur

--- script_create.sql
+++ script_create.sql
@@ -19,7 +19,7 @@ CREATE TABLE utilisateur
     nom VARCHAR(255) NOT NULL,
     prenom VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    mot_de_passe VARCHAR(15) NOT NULL,
+    mot_de_passe CHAR(60) NOT NULL,
     admin BOOLEAN
 );
 
